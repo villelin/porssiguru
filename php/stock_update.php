b2d2dba92aa7113b84d8799352eb2f11ef744c61
villelin <villelin@metropolia.fi>
2017-11-21 21:37:05 +0200

    pörssidata tietokantaan

--- php/stock_update.php
+++ php/stock_update.php
@@ -5,24 +5,51 @@
  * Date: 19.11.2017
  * Time: 19.29
  *
- * Parsii pörssitiedot Kauppalehden pörssisivuilta
  */
 
+require_once('config.php');
 
+// Alpha Vantage API:n käyttöavain
+define("apikey", "F07XVL4DE169DFQY");
+
+// haettavien valuuttojen lista
+$currency_type_list = array();
+$currency_type_list[] = array('symbol' => 'XAU', 'name' => 'Kulta (unssi)');
+$currency_type_list[] = array('symbol' => 'XAG', 'name' => 'Hopea (unssi)');
+$currency_type_list[] = array('symbol' => 'BTC', 'name' => 'Bitcoin');
+$currency_type_list[] = array('symbol' => 'BCH', 'name' => 'Bitcoin-Cash');
+$currency_type_list[] = array('symbol' => 'ETH', 'name' => 'Ethereum');
+$currency_type_list[] = array('symbol' => 'DOGE', 'name' => 'Dogecoin');
+$currency_type_list[] = array('symbol' => 'LTC', 'name' => 'Litecoin');
+$currency_type_list[] = array('symbol' => 'XMR', 'name' => 'Monero');
+$currency_type_list[] = array('symbol' => 'XRP', 'name' => 'Ripples');
+$currency_type_list[] = array('symbol' => 'DASH', 'name' => 'Dash');
+
+
+
+
+
+$time_start = microtime(true);
 
 // Helsinki lista
-$xhel_list = parseStockData("https://beta.kauppalehti.fi/porssi/kurssit/XHEL");
+$xhel_list = parseStockData("https://beta.kauppalehti.fi/porssi/kurssit/XHEL", "Helsinki");
 // First North lista
-$fnfi_list = parseStockData("https://beta.kauppalehti.fi/porssi/kurssit/FNFI");
+$fnfi_list = parseStockData("https://beta.kauppalehti.fi/porssi/kurssit/FNFI", "First North");
+// Valuutat
+$currencies = getCurrencyData($currency_type_list, "Valuutta");
+
+$time_end = microtime(true);
+
+$total_time = $time_end - $time_start;
+echo "Haku vei aikaa " . $total_time . " sekuntia<br>";
+
+// listat yhteen
+$full_list = array_merge($xhel_list, $fnfi_list, $currencies);
+
+// päivitetään tietokanta
+updateStockDatabase($DBH, $full_list);
 
 
-echo "Helsinki: " . count($xhel_list) . " yhtiötä<br>";
-echo "First North: " . count($fnfi_list) . " yhtiötä<br>";
-
-$full_list = array_merge($xhel_list, $fnfi_list);
-
-//$json = json_encode($full_list);
-//echo $json;
 
 echo "<table>";
 echo "<tr>";
@@ -30,6 +57,7 @@ echo "<th>Symboli</th>";
 echo "<th>Yhtiö</th>";
 echo "<th>Hinta</th>";
 echo "<th>Muutos</th>";
+echo "<th>Kategoria</th>";
 echo "</tr>";
 foreach ($full_list as $entry)
 {
@@ -38,11 +66,17 @@ foreach ($full_list as $entry)
     echo "<td>" . $entry["company"] . "</td>";
     echo "<td>" . $entry["price"] . "</td>";
     echo "<td>" . $entry["change"] . "</td>";
+    echo "<td>" . $entry["category"] . "</td>";
     echo "</tr>";
 }
 echo "</table>";
 
-function parseStockData($url)
+
+
+/*
+ * Parsii pörssitiedot Kauppalehden pörssisivuilta
+ */
+function parseStockData($url, $market)
 {
     $stock_list = array();
 
@@ -131,13 +165,18 @@ function parseStockData($url)
                             // jos joku on null, jotain meni pieleen
                             if ($company_name != null && $price != null && $change != null && $stock_symbol != null)
                             {
-                                $stock_entry = array();
-                                $stock_entry["symbol"] = $stock_symbol;
-                                $stock_entry["company"] = $company_name;
-                                $stock_entry["price"] = $price;
-                                $stock_entry["change"] = $change;
+                                // hinnan pitää olla suurempi kuin 0
+                                if ($price > 0)
+                                {
+                                    $stock_entry = array();
+                                    $stock_entry["symbol"] = $stock_symbol;
+                                    $stock_entry["company"] = $company_name;
+                                    $stock_entry["price"] = $price;
+                                    $stock_entry["change"] = $change;
+                                    $stock_entry["category"] = $market;
 
-                                $stock_list[] = $stock_entry;
+                                    $stock_list[] = $stock_entry;
+                                }
                             }
                         }
                     }
@@ -149,15 +188,115 @@ function parseStockData($url)
         else
         {
             echo "HTML ei aukea";
-            return null;
         }
     }
     else
     {
         echo "URL ei aukea";
-        return null;
     }
+
+    return $stock_list;
 }
 
+
+/*
+ * Parsii valuuttatiedot Alpha Vantagen API:sta
+ */
+function getCurrencyData($entry_list, $category)
+{
+    $currency_list = array();
+
+    foreach ($entry_list as $entry)
+    {
+        // luodaan Alpha Vantage URL
+        // valuutasta euroksi
+        $url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE";
+        $url .= "&from_currency=" . $entry["symbol"];
+        $url .= "&to_currency=EUR";
+        $url .= "&apikey=" . apikey;
+
+        // JSON serveriltä
+        $json = file_get_contents($url);
+        if ($json != false)
+        {
+            $data = json_decode($json, true);
+
+            $rate = $data["Realtime Currency Exchange Rate"];
+
+            $price = $rate["5. Exchange Rate"];
+
+            // hinnan pitää olla suurempi kuin 0
+            if ($price > 0)
+            {
+                $currency_entry = array();
+                $currency_entry["symbol"] = $entry["symbol"];
+                $currency_entry["company"] = $entry["name"];
+                $currency_entry["price"] = $price;
+                $currency_entry["change"] = "0";        // valuutoille ei ole muutosprosenttia tarjolla
+                $currency_entry["category"] = $category;
+
+                $currency_list[] = $currency_entry;
+            }
+        }
+    }
+
+    return $currency_list;
+}
+
+
+/*
+ * Lähettää listan tiedot tietokantaan
+ */
+function updateStockDatabase($dbh, $stock_list)
+{
+    foreach ($stock_list as $entry)
+    {
+        $symbol = $entry["symbol"];
+        $name = $entry["company"];
+        $price = $entry["price"];
+        $change = $entry["change"];
+        $category = $entry["category"];
+
+        // onko yhtiö/valuutta jo olemassa
+        $find_query = "SELECT symbol FROM stock WHERE symbol='$symbol'";
+
+        $sql = $dbh->prepare($find_query);
+        $sql->execute();
+
+        // vakiona päivitetään
+        $update = true;
+
+        try
+        {
+            if ($sql->rowCount() == 0)
+            {
+                // ei löytynyt, lisätään
+                $update = false;
+            }
+        }
+        catch (PDOException $e)
+        {
+            die("VIRHE: " . $e->getMessage());
+        }
+
+        $update_query = "";
+
+        // query päivitykselle tai lisäykselle
+        if ($update)
+        {
+            // päivitetään
+            $update_query = "UPDATE stock SET price=$price, variety=$change WHERE symbol='$symbol'";
+        }
+        else
+        {
+            // lisätään
+            $update_query = "INSERT INTO stock(symbol, company, price, variety, category)VALUES('$symbol', '$name', '$price', '$change', '$category')";
+        }
+
+        // tiedot kantaan
+        $sql = $dbh->prepare($update_query);
+        $sql->execute();
+    }
+}
 
 ?>
