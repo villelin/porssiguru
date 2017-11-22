f33c930b712a9a216c7f7eae365aa7c69541fca1
villelin <villelin@metropolia.fi>
2017-11-22 23:10:50 +0200

    sessiot toimii Ajaxin yli

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@ const response_element = document.querySelector("#response");
 const loginSend = ((evt) => {
   evt.preventDefault();
 
-  const username_element = document.querySelector('input[name="username"]');
-  const password_element = document.querySelector('input[name="password"]');
+  const username_element = document.querySelector('input[name="log_username"]');
+  const password_element = document.querySelector('input[name="log_password"]');
 
   const data = new FormData();
   data.append('username', username_element.value);
   data.append('password', password_element.value);
 
-  const settings = { method: 'POST', body: data };
+  const settings = { method: 'POST', body: data, cache: 'no-cache', credentials: 'include' };
 
   fetch('php/register.php', settings).then((response) => {
     if (response.status !== 200) {
@@ -26,6 +26,46 @@ const loginSend = ((evt) => {
   });
 });
 
+const nappi_response = document.querySelector("#nappi_response");
+const nappiTest = ((evt) => {
+  evt.preventDefault();
 
+  const settings = { method: 'POST', cache: 'no-cache', credentials: 'include' };
+
+  fetch('php/test.php', settings).then((response) => {
+    if (response.status !== 200) {
+      nappi_response.innerHTML = "Ei toimi";
+    } else {
+      response.json().then((data) => {
+        nappi_response.innerHTML = data.result;
+      });
+    }
+  }).catch((error) => {
+    nappi_response.innerHTML = "NYT FEILAS PAHASTI";
+  });
+});
+
+const nappiLogout = ((evt) => {
+  evt.preventDefault();
+
+  const settings = { method: 'POST', cache: 'no-cache', credentials: 'include' };
+
+  fetch('php/logout.php', settings).then((response) => {
+    if (response.status !== 200) {
+      response_element.innerHTML = "Ei toimi";
+    } else {
+      response.json().then((data) => {
+        response_element.innerHTML = data.result;
+      });
+    }
+  }).catch((error) => {
+    response_element.innerHTML = "NYT FEILAS PAHASTI";
+  });
+});
+
+
+document.querySelector("#nappi").addEventListener('click', nappiTest);
+document.querySelector("#logout").addEventListener('click', nappiLogout);
 document.querySelector("form").addEventListener('submit', loginSend);
+
 
--- php/register.php
+++ php/register.php
@@ -6,6 +6,8 @@
  * Time: 11.22
  */
 
+require_once('session.php');
+
 $username = "TYHJÄ";
 $password = "TYHJÄ";
 
@@ -17,7 +19,21 @@ if (isset($_POST["password"])) {
 }
 
 $response = array();
-$response[] = "Username oli " . $username . ". Password oli " . $password;
+
+if ($username === "Masa") {
+    $_SESSION["username"] = $username;
+    $_SESSION["password"] = $password;
+    $_SESSION["logged_in"] = true;
+    $response[] = "Masa loggas sisään";
+} else {
+    $_SESSION["username"] = $username;
+    $_SESSION["password"] = $password;
+    $_SESSION["logged_in"] = true;
+    $response[] = "Username oli " . $username . ". Password oli " . $password;
+}
+
+
+
 
 $json = json_encode($response);
 echo $json;
