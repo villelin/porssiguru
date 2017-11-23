a6cf1e332616fa9b7941eb801d2bd247443bd26b
villelin <villelin@metropolia.fi>
2017-11-23 13:50:13 +0200

    loggautumisen tallentaminen

--- php/login.php
+++ php/login.php
@@ -8,6 +8,12 @@
 
 define('SALT','PörssigurunSalasananSuolaus40t9ert0e9rt8er');
 
+// virhetilat kirjautumiselle
+define('LOGIN_OK', 1);
+define('LOGIN_WRONG_PASS', 2);
+define('LOGIN_NO_ACCOUNT', 3);
+define('LOGIN_DB_ERROR', 4);
+
 require_once('session.php');
 require_once('config.php');
 
@@ -19,6 +25,46 @@ if (isset($_POST["username"]) && isset($_POST["password"])) {
 
     $hashed_password = hash('sha256', $password.SALT);
 
+    $result = tryLogin($DBH, $username, $hashed_password);
+    switch ($result) {
+        case LOGIN_OK: {
+            // salasana täsmää, logataan sisään
+            $_SESSION["username"] = $username;
+            $_SESSION["password"] = $password;
+            $_SESSION["logged_in"] = true;
+            // TODO: lisää sessioparametrejä?
+
+            insertLogin($DBH, $username);
+
+            // vastaus Ajaxille
+            $response["error"] = false;
+            $response["message"] = "Sisäänkirjautuminen onnistui.";
+            break;
+        }
+        case LOGIN_WRONG_PASS: {
+            // salasana ei täsmää
+
+            // vastaus Ajaxille
+            $response["error"] = true;
+            $response["message"] = "Väärä salasana.";
+            break;
+        }
+        case LOGIN_NO_ACCOUNT: {
+            // käyttäjätunnusta ei löytynyt
+
+            // vastaus Ajaxille
+            $response["error"] = true;
+            $response["message"] = "Käyttäjätunnusta $username ei löydy.";
+            break;
+        }
+        case LOGIN_DB_ERROR: {
+            $response["error"] = true;
+            $response["message"] = "Tietokantavirhe.";
+            break;
+        }
+    }
+
+    /*
     $query = "SELECT id, username, pass FROM user_account WHERE username='$username'";
     $sql = $DBH->prepare($query);
     $sql->execute();
@@ -37,6 +83,8 @@ if (isset($_POST["username"]) && isset($_POST["password"])) {
                 $_SESSION["user_id"] = $row["id"];
                 $_SESSION["logged_in"] = true;
                 // TODO: lisää sessioparametrejä?
+
+
 
                 // vastaus Ajaxille
                 $response["error"] = false;
@@ -59,6 +107,7 @@ if (isset($_POST["username"]) && isset($_POST["password"])) {
         $response["error"] = true;
         $response["message"] = "Tietokantavirhe.";
     }
+    */
 } else {
     $response["error"] = true;
     $response["message"] = "";
@@ -72,3 +121,64 @@ if (isset($_POST["username"]) && isset($_POST["password"])) {
 
 $json = json_encode($response);
 echo $json;
+
+
+
+function tryLogin($dbh, $username, $hashed_password) {
+    $query = "SELECT id, username, pass FROM user_account WHERE username='$username'";
+    $sql = $dbh->prepare($query);
+    $sql->execute();
+
+    try {
+        if ($sql->rowCount() != 0) {
+            // käyttäjätunnus löytyi
+
+            $row = $sql->fetch();
+
+            // täsmääkö salasana?
+            if ($row["pass"] === $hashed_password) {
+                return LOGIN_OK;
+            } else {
+                return LOGIN_WRONG_PASS;
+            }
+        } else {
+            return LOGIN_NO_ACCOUNT;
+        }
+    } catch (PDOException $e) {
+        return LOGIN_DB_ERROR;
+    }
+}
+
+/*
+ * Lisää uusi sisään loggautumine tauluun
+ */
+
+function insertLogin($dbh, $username) {
+    // hae user id
+    $user_query = "SELECT id FROM user_account WHERE username='$username'";
+    $sql = $dbh->prepare($user_query);
+    $sql->execute();
+
+    $user_id = null;
+
+    try {
+        if ($sql->rowCount() != 0) {
+            // löytyi
+
+            $row = $sql->fetch();
+            $user_id = $row["id"];
+        } else {
+            return;
+        }
+    } catch (PDOException $e) {
+        return;
+    }
+
+    if ($user_id != null) {
+        // lisää uusi login
+
+        $login_query = "INSERT INTO user_login(user_id, login)VALUES('$user_id', CURRENT_TIMESTAMP())";
+        $sql = $dbh->prepare($login_query);
+        $sql->execute();
+    }
+}
--- php/logout.php
+++ php/logout.php
@@ -7,14 +7,58 @@
  */
 
 require_once('session.php');
+require_once('config.php');
 
 $response = array();
+
+if (isset($_SESSION["logged_in"])) {
+    $username = $_SESSION["username"];
+
+    // lisää ulosloggautuminen tauluun
+    insertLogout($DBH, $username);
+
+    $response["error"] = false;
+    $response["message"] = "$username loggas ulos";
+} else {
+    $response["error"] = false;
+    $response["message"] = "Ei oltu sisällä";
+}
 
 session_unset();
 session_destroy();
 
-$response["error"] = false;
-$response["message"] = "Loggas ulos";
-
 $json = json_encode($response);
 echo $json;
+
+
+/*
+ * Lisää uusi ulos loggautuminen tauluun
+ */
+function insertLogout($dbh, $username) {
+    // hae viimeisin login
+    $latest_query = "SELECT l.id FROM user_account AS u, user_login AS l WHERE u.username='$username' AND u.id=l.user_id ORDER BY l.login DESC LIMIT 1";
+    $sql = $dbh->prepare($latest_query);
+    $sql->execute();
+
+    $last_id = null;
+
+    try {
+        if ($sql->rowCount() != 0) {
+            // löytyi
+
+            $row = $sql->fetch();
+            $last_id = $row[0];
+        } else {
+            return;
+        }
+    } catch (PDOException $e) {
+        return;
+    }
+
+    // päivitä uloskirjautumisaika
+    if ($last_id != null) {
+        $logout_query = "UPDATE user_login SET logout=CURRENT_TIMESTAMP() WHERE id='$last_id'";
+        $sql = $dbh->prepare($logout_query);
+        $sql->execute();
+    }
+}
