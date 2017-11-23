c73839f604b099d0fd7a1c4938c23aa97512fa32
villelin <villelin@metropolia.fi>
2017-11-23 12:33:05 +0200

    rekisteröinti toimii

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
+
+const login_form = document.querySelector("#login_form");
+const register_form = document.querySelector("#register_form");
+
 const response_element = document.querySelector("#response");
-const login_form = document.querySelector("form");
+const nappi_response = document.querySelector("#nappi_response");
+const reg_response = document.querySelector("#reg_response");
 
 const loginSend = ((evt) => {
   evt.preventDefault();
@@ -33,7 +38,7 @@ const loginSend = ((evt) => {
   });
 });
 
-const nappi_response = document.querySelector("#nappi_response");
+
 const nappiTest = ((evt) => {
   evt.preventDefault();
 
@@ -56,6 +61,7 @@ const nappiTest = ((evt) => {
     nappi_response.innerHTML = "NYT FEILAS PAHASTI";
   });
 });
+
 
 const nappiLogout = ((evt) => {
   evt.preventDefault();
@@ -81,8 +87,44 @@ const nappiLogout = ((evt) => {
 });
 
 
+const registerSend = ((evt) => {
+  evt.preventDefault();
+
+  const username_element = document.querySelector('input[name="reg_username"]');
+  const password_element = document.querySelector('input[name="reg_password"]');
+  const email_element = document.querySelector('input[name="reg_email"]');
+
+  const data = new FormData();
+  data.append('username', username_element.value);
+  data.append('password', password_element.value);
+  data.append('email', email_element.value);
+
+  const settings = { method: 'POST', body: data, cache: 'no-cache', credentials: 'include' };
+
+  fetch('php/register.php', settings).then((response) => {
+    if (response.status !== 200) {
+      reg_response.innerHTML = "Palvelu ei käytössä";
+    }
+    else {
+      response.json().then((data) => {
+        let message = "";
+        if (data.error == true) {
+          message += "VIRHE: ";
+        }
+        message += data.message;
+        register_form.reset();
+        reg_response.innerHTML = message;
+      });
+    }
+  }).catch((error) => {
+    reg_response.innerHTML = "FEILAS PAHASTI";
+  });
+});
+
+
 document.querySelector("#nappi").addEventListener('click', nappiTest);
 document.querySelector("#logout").addEventListener('click', nappiLogout);
-document.querySelector("form").addEventListener('submit', loginSend);
+document.querySelector("#login_form").addEventListener('submit', loginSend);
+document.querySelector("#register_form").addEventListener('submit', registerSend);
 
 
--- php/register.php
+++ php/register.php
@@ -6,8 +6,12 @@
  * Time: 11.22
  */
 
+require_once('config.php');
 require_once('session.php');
 
+define('SALT','PörssigurunSalasananSuolaus40t9ert0e9rt8er');
+
+/*
 $username = "TYHJÄ";
 $password = "TYHJÄ";
 
@@ -31,11 +35,67 @@ if ($username === "Masa") {
     $_SESSION["logged_in"] = true;
     $response[] = "Username oli " . $username . ". Password oli " . $password;
 }
+*/
+
+$response = array();
+
+if (isset($_POST["username"]) && isset($_POST["password"]) && isset($_POST["email"])) {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $email = $_POST["email"];
+
+    // TODO: validoi tiedot
+
+    // onko käyttäjätunnus jo olemassa?
+    $exists = doesUsernameExist($DBH, $username);
+    if ($exists === null) {
+        // tietokantavirhe
+    } else if ($exists) {
+        // on olemassa
+        // vastaus Ajaxille
+        $response["error"] = true;
+        $response["message"] = "Käyttäjätunnus $username on jo olemassa.";
+    } else {
+        // ei ole
+        $hashed_password = hash('sha256', $password.SALT);
+
+        registerUsername($DBH, $username, $hashed_password, $email);
+
+        // vastaus Ajaxille
+        $response["error"] = false;
+        $response["message"] = "Käyttäjätunnuksen luonti onnistui.";
+    }
+} else {
+    $response["error"] = true;
+    $response["message"] = "Rekisteröinti ei toimi.";
+}
 
 
 
 
 $json = json_encode($response);
 echo $json;
+
+function doesUsernameExist($dbh, $username) {
+    $find_query = "SELECT id FROM user_account WHERE username='$username'";
+    $sql = $dbh->prepare($find_query);
+    $sql->execute();
+
+    try {
+        if ($sql->rowCount() == 0) {
+            return false;
+        } else {
+            return true;
+        }
+    } catch (PDOException $e) {
+        return null;
+    }
+}
+
+function registerUsername($dbh, $username, $password, $email) {
+    $insert_query = "INSERT INTO user_account(username, email, pass, signup_date)VALUES('$username', '$email', '$password', CURRENT_TIMESTAMP)";
+    $sql = $dbh->prepare($insert_query);
+    $sql->execute();
+}
 
 ?>
