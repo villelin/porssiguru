85e4f3ba50d9bc3a897ff3575fd1371c4025cb4c
elinaro <elina.royhy@metropolia.fi>
2017-11-27 09:28:09 +0200

    Merge branch 'master' of /Volumes/elinaro/public_html/PG/porssiguru with conflicts.

--- js/main.js
+++ js/main.js
@@ -1,31 +1,24 @@
 const response_element = document.querySelector("#response");
-const login_form = document.querySelector("form");
 
 const loginSend = ((evt) => {
   evt.preventDefault();
 
-  const username_element = document.querySelector('input[name="log_username"]');
-  const password_element = document.querySelector('input[name="log_password"]');
+  const username_element = document.querySelector('input[name="username"]');
+  const password_element = document.querySelector('input[name="password"]');
 
   const data = new FormData();
   data.append('username', username_element.value);
   data.append('password', password_element.value);
 
-  const settings = { method: 'POST', body: data, cache: 'no-cache', credentials: 'include' };
+  const settings = { method: 'POST', body: data };
 
-  fetch('php/login.php', settings).then((response) => {
+  fetch('php/register.php', settings).then((response) => {
     if (response.status !== 200) {
-      response_element.innerHTML = "Palvelu ei käytössä";
+      response_element.innerHTML = "Ei toimi";
     }
     else {
       response.json().then((data) => {
-        let message = "";
-        if (data.error == true) {
-          message += "VIRHE: ";
-        }
-        message += data.message;
-        login_form.reset();
-        response_element.innerHTML = message;
+        response_element.innerHTML = data;
       });
     }
   }).catch((error) => {
@@ -33,56 +26,10 @@ const loginSend = ((evt) => {
   });
 });
 
-const nappi_response = document.querySelector("#nappi_response");
-const nappiTest = ((evt) => {
-  evt.preventDefault();
-
-  const settings = { method: 'POST', cache: 'no-cache', credentials: 'include' };
-
-  fetch('php/test.php', settings).then((response) => {
-    if (response.status !== 200) {
-      nappi_response.innerHTML = "Ei toimi";
-    } else {
-      response.json().then((data) => {
-        let message = "";
-        if (data.error == true) {
-          message += "VIRHE: ";
-        }
-        message += data.message;
-        nappi_response.innerHTML = message;
-      });
-    }
-  }).catch((error) => {
-    nappi_response.innerHTML = "NYT FEILAS PAHASTI";
-  });
-});
-
-const nappiLogout = ((evt) => {
-  evt.preventDefault();
-
-  const settings = { method: 'POST', cache: 'no-cache', credentials: 'include' };
-
-  fetch('php/logout.php', settings).then((response) => {
-    if (response.status !== 200) {
-      response_element.innerHTML = "Ei toimi";
-    } else {
-      response.json().then((data) => {
-        let message = "";
-        if (data.error == true) {
-          message += "VIRHE: ";
-        }
-        message += data.message;
-        response_element.innerHTML = message;
-      });
-    }
-  }).catch((error) => {
-    response_element.innerHTML = "NYT FEILAS PAHASTI";
-  });
-});
-
-
-document.querySelector("#nappi").addEventListener('click', nappiTest);
-document.querySelector("#logout").addEventListener('click', nappiLogout);
 document.querySelector("form").addEventListener('submit', loginSend);
 
+document.querySelector("uusi").addEventListener("click", function(){
 
+  document.querySelector("testi").innerHTML()
+
+});
