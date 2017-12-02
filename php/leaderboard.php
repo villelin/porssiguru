8b2ae1670ed92e8c3af06bc6d28e7c9c45d57a10
villelin <villelin@metropolia.fi>
2017-12-02 20:40:35 +0200

    leaderboard toimii

--- php/leaderboard.php
+++ php/leaderboard.php
@@ -8,47 +8,99 @@
 
 require_once('config.php');
 
+function leaderboardCompare($a, $b) {
+    if ($a["assets"] > $b["assets"]) {
+        return -1;
+    } else if($a["assets"] < $b["assets"]) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
 $response = array();
 
-$query = "SELECT username, user_id, IFNULL(stocksum.assets,0)+u.funds AS 'assets'
-          FROM
-          (SELECT user_id, SUM(buy_sum-sell_sum) AS 'assets'
+$num = 0;
+if (isset($_POST['num'])) {
+    $num = $_POST['num'];
+}
+
+$user_assets = array();
+
+// käyttäjien osakeomistukset
+$query = "SELECT user_id, SUM(assets)
+          FROM(
+          SELECT user_id, stock.company, (buy_sum-sell_sum)*stock.price AS 'assets'
           FROM(
            SELECT user_id, stock_id, SUM(buy) AS buy_sum, SUM(sell) AS sell_sum
            FROM(
-           (SELECT user_id, stock_id, amount*s.price  AS 'buy', 0 AS 'sell'
-            FROM stock_event, stock AS s
-            WHERE transaction_type='Buy' AND s.id=stock_id)
+           (SELECT user_id, stock_id, amount AS 'buy', 0 AS 'sell'
+            FROM stock_event
+            WHERE transaction_type='Buy')
             UNION ALL
-           (SELECT user_id, stock_id, 0 AS 'buy', amount*s.price AS 'sell'
-            FROM stock_event, stock AS s
-            WHERE transaction_type='Sell' AND s.id=stock_id)
+           (SELECT user_id, stock_id, 0 AS 'buy', amount AS 'sell'
+            FROM stock_event
+            WHERE transaction_type='Sell')
            ) AS summed
            GROUP BY user_id, stock_id
-          ) AS final, stock, user_account AS u
-          WHERE final.stock_id=stock.id AND user_id=u.id
-          GROUP BY user_id) AS stocksum, user_account AS u
-          WHERE u.id=stocksum.user_id
-          ORDER BY assets DESC";
-
-if (isset($_POST["num"])) {
-    $num = $_POST["num"];
-    $query .= " LIMIT $num";
-}
+          ) AS final, stock
+          WHERE final.stock_id=stock.id
+          ) AS a
+          GROUP BY user_id";
 
 $sql = $DBH->prepare($query);
 $sql->execute();
 
-try
-{
+try {
     while ($row = $sql->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)) {
-        $response[] = array("user_id" => $row[0], "username" => $row[1], "assets" => $row[2]);
+        $user_id = $row[0];
+        $assets = $row[1];
+
+        $user_assets["$user_id"]["stock"] = $assets;
     }
 }
-catch (PDOException $e)
-{
-    $response[] = "";
+catch (PDOException $e) {
 }
 
-$json = json_encode($response);
+// käyttäjien käteiset
+$query = "SELECT id, username, funds FROM user_account";
+$sql = $DBH->prepare($query);
+$sql->execute();
+
+$user_funds = array();
+
+try {
+    while ($row = $sql->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)) {
+        $user_id = $row[0];
+        $username = $row[1];
+        $funds = $row[2];
+
+        $user_assets["$user_id"]["funds"] = $funds;
+        $user_assets["$user_id"]["username"] = $username;
+    }
+}
+catch (PDOException $e) {
+}
+
+$leaderboard = array();
+
+foreach ($user_assets as $user_id => $item) {
+    $stock = 0;
+    $funds = 0;
+    if (isset($item["stock"])) {
+        $stock = $item["stock"];
+    }
+    if (isset($item["funds"])) {
+        $funds = $item["funds"];
+    }
+
+    $assets = $stock + $funds;
+
+    $leaderboard[] = array("user_id" => $user_id, "username" => $item["username"], "assets" => $assets);
+}
+
+// järjestetään assettien mukaan laskevasti
+usort($leaderboard, "leaderboardCompare");
+
+$json = json_encode($leaderboard);
 echo $json;
