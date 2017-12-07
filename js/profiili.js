4743292a85c33e2719002d1a88df1d0d823570ff
elinaro <elina.royhy@metropolia.fi>
2017-12-07 15:21:13 +0200

    html&css

--- js/profiili.js
+++ js/profiili.js
@@ -40,10 +40,10 @@ const updateProfile = (() => {
         vaihtokuva.src = imageurl;
 
         let comments = "";
-        comments += "<table>";
+        comments += "<table class='profTable'>";
         data.comments.forEach((item) => {
           comments += "<tr>";
-          comments += `<td><strong>${item.username}</strong></td><td>${item.text}</td><td>${item.date}</td>`;
+          comments += `<td class="profTd"><strong>${item.username}</strong></td><td class="profTd" >${item.text}</td><td class="profTd">${item.date}</td>`;
           comments += "</tr>";
         });
         comments += "</table>";
