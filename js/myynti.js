fc738fe6fae04baf59f629505b1d5007261c2583
villelin <villelin@metropolia.fi>
2017-12-07 14:23:09 +0200

    tyhjät omistukset

--- js/myynti.js
+++ js/myynti.js
@@ -39,32 +39,38 @@ const myyLista = (() => {
         if (data != null) {
           const funds = data.funds;
           let html="";
-          data.stock.forEach((item, index) => {
-            const stock_id = item.stock_id;
-            const company = item.company;
-            let price = parseFloat(item.price);
-            price = price.toLocaleString('fi-FI', { style: 'currency', currency: 'EUR' });
-            const amount = item.assets;
+          if (data.stock != null) {
+            data.stock.forEach((item, index) => {
+              const stock_id = item.stock_id;
+              const company = item.company;
+              let price = parseFloat(item.price);
+              price = price.toLocaleString('fi-FI',
+                  {style: 'currency', currency: 'EUR'});
+              const amount = item.assets;
 
-            const sell_min = 1;
-            const sell_max = amount;
+              const sell_min = 1;
+              const sell_max = amount;
 
-            html += `<tr>`;
-            html += `<td id="osake">${company}</td>`;
-            html += `<td id="hinta">${price}</td>`;
-            html += `<td id="maara">${amount}</td>`;
-            html += `<td>`;
-            html += `<form id="sellform" method="POST" onsubmit="myyEvent(event, ${stock_id})">`;
-            html += `<input type="number" name="sell_amount" min="${sell_min}" max="${sell_max}">`;
-            html += `<input type="submit" value="Myy">`;
-            html += `</form>`;
-            html += `</td>`;
-            html += `</tr>`;
+              html += `<tr>`;
+              html += `<td id="osake">${company}</td>`;
+              html += `<td id="hinta">${price}</td>`;
+              html += `<td id="maara">${amount}</td>`;
+              html += `<td>`;
+              html += `<form id="sellform" method="POST" onsubmit="myyEvent(event, ${stock_id})">`;
+              html += `<input type="number" name="sell_amount" min="${sell_min}" max="${sell_max}">`;
+              html += `<input type="submit" value="Myy">`;
+              html += `</form>`;
+              html += `</td>`;
+              html += `</tr>`;
 
-            //html += `<tr id="topRivi" onclick="openProfile(${id})"><td class="sija">${index + 1}</td><td class="kuva"><img src="${urli}"></td><td class="kayttaja">${name}</td><td class="nettovarat">${assets} </td></tr>`;
-          });
-          myylista.innerHTML = "<tr><th>Osake</th><th>Hinta/kpl</th><th>Määrä</th><th>Myy kpl</th></tr>" + html;
-          //toplista.innerHTML = `<tr class="topOtsikko" ><td class="otsikkosija">SIJA</td><td>KÄYTÄJÄ</td><td></td><td class="otsikkonettovarat">NETTOVARAT</td></tr>` + html;
+              //html += `<tr id="topRivi" onclick="openProfile(${id})"><td class="sija">${index + 1}</td><td class="kuva"><img src="${urli}"></td><td class="kayttaja">${name}</td><td class="nettovarat">${assets} </td></tr>`;
+            });
+            myylista.innerHTML = "<tr><th>Osake</th><th>Hinta/kpl</th><th>Määrä</th><th>Myy kpl</th></tr>" +
+                html;
+            //toplista.innerHTML = `<tr class="topOtsikko" ><td class="otsikkosija">SIJA</td><td>KÄYTÄJÄ</td><td></td><td class="otsikkonettovarat">NETTOVARAT</td></tr>` + html;
+          } else {
+            myylista.innerHTML = "Et omista tällä hetkellä mitään.";
+          }
         }
       });
     } else {
